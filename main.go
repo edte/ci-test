a979dc8432c7d9904ba732b920640c0c4203ee0c
edte <zzzzip6@gmail.com>
2020-08-13 21:20:35 +0800

    core: add .travis.yml

--- main.go
+++ main.go
@@ -3,4 +3,17 @@
 // @create: 2020-08-13 20:58
 package main
 
+import (
+	"net/http"
+)
 
+type A struct {
+}
+
+func (a A) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("hello world"))
+}
+
+func main() {
+	http.ListenAndServe(":8080", A{})
+}
